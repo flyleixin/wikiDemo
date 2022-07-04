f1d76233a54837bb7cf282887d57674c222b8f24
“leixin” <hello@itcast.cn~>
2022-07-04 20:06:12 +0800

    init2

---
+++ src/test/java/com/leixin/wikidemo/Test2.java
@@ -0,0 +1,12 @@
+package com.leixin.wikidemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test2 {
+    @Test
+    public void testOne(){
+        System.out.println("hello");
+    }
+}
--- src/test/java/com/leixin/wikidemo/WikiDemoApplicationTests.java
+++ src/test/java/com/leixin/wikidemo/WikiDemoApplicationTests.java
@@ -8,6 +8,7 @@ class WikiDemoApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("git");
     }
 
 }
